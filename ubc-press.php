1fbbe43805a19b175ba1cd93fa0f5917bd08c062
Richard Tape <richard.tape@ubc.ca>
2015-05-25 11:14:54 -0700

    ADD initial commit (literally just plugin headers)

---
+++ ubc-press.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ *
+ * @wordpress-plugin
+ * Plugin Name:       UBC Press
+ * Plugin URI:        http://ctlt.ubc.ca/
+ * Description:       A plugin to help the WP dashboard look and feel more like a teaching and learning platform
+ * Version:           1.0.0
+ * Author:            Richard Tape
+ * Author URI:        http://blogs.ubc.ca/mbcx9rvt
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       ubc-press
+*/
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+class UBC_Press {
+
+	public static function init() {
+
+
+
+	}/* init() */
+
+}/* class UBC_Press */
