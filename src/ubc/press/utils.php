5514848cde9b0bfdf701b9c7ca5409dac96a1ae7
Richard Tape <richard@iamfriendly.com>
2015-11-12 14:46:00 -0800

    ADDED utils for program and course objectives getters from the main network site

--- src/ubc/press/utils.php
+++ src/ubc/press/utils.php
@@ -437,4 +437,237 @@ class Utils {
 
 	}/* is_timestamp() */
 
+
+	/**
+	 * Get course objectives.
+	 * "Usable" means return an array where the keys are the term IDs
+	 * and the values are the term names
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param (bool) $usable - Whether this should be a simple term_id=>term_name array
+	 * @return (array) An array of terms
+	 */
+
+	public static function get_course_objectives( $usable = false ) {
+
+		$course_objectives = get_terms( 'course-objective', array( 'hide_empty' => 0 ) );
+
+		if ( false === $usable ) {
+			return $course_objectives;
+		}
+
+		// If it's empty, bail early with an empty array
+		if ( empty( $course_objectives ) ) {
+			return array();
+		}
+
+		$usable_course_objectives = array();
+		foreach ( $course_objectives as $id => $term_details ) {
+			$usable_course_objectives[ $term_details->term_id ] = $term_details->name;
+		}
+
+		return $usable_course_objectives;
+
+	}/* get_course_objectives */
+
+
+	/**
+	 * Program objectives are set at the network level. When a new network is created
+	 * That network has a 'main site'. That main site has a CPT called programs. And
+	 * those programs have a taxonomy called program-objectives. When a course site
+	 * is created, we ask them what program the course belongs to. This allows us to
+	 * know which program objectives are available at a course level
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param bool) $usable - Whether this should be a simple term_id=>term_name array
+	 * @return (array) An array of terms
+	 */
+
+	public static function get_program_objectives( $usable = false ) {
+
+		// First we need to get the main site for the network of the current blog
+		$main_site_id 	= \UBC\Press\Utils::get_id_for_networks_main_site_of_blog_id( get_current_blog_id() );
+
+		// Now we need to know what program the current blog is attached to. That's stored
+		// in the onboarding options ubc_press_course_details::program
+		$course_details	= get_option( 'ubc_press_course_details' );
+		$program 		= $course_details['program'];
+
+		// Now we have that, we can look at the terms assoc
+		$program_objectives = static::get_program_objectives_for_post_of_site( $program, $main_site_id );
+
+		if ( false === $usable ) {
+			return $program_objectives;
+		}
+
+		$usable_program_objectives = array();
+
+		foreach ( $program_objectives as $id => $term_obj ) {
+			$usable_program_objectives[ $term_obj->term_id ] = $term_obj->name;
+		}
+
+		return $usable_program_objectives;
+
+	}/* get_program_objectives() */
+
+
+	/**
+	 * Fetch the program objectives for a post on the given site.
+	 * Programs are a post type on the main network site. Program objectives are
+	 * a taxonomy of that post type
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param (int) $post_id - The ID of the post for which we're looking for the associated terms
+	 * @param (int) $site_id - The site on which this post lives (will be a main site ID)
+	 * @return (array|false) - An array of terms (program objectives)
+	 */
+
+	public static function get_program_objectives_for_post_of_site( $post_id, $site_id ) {
+
+		// Now fetch a list of programs from the main site
+		global $wpdb;
+
+		$post_id = absint( $post_id );
+		$site_id = absint( $site_id );
+
+		$blog_prefix	= $wpdb->get_blog_prefix( $site_id );
+
+		// First get the term_tax_ids for this post
+		$query			= "SELECT term_taxonomy_id FROM {$blog_prefix}term_relationships WHERE object_id = %d";
+		$query 			= $wpdb->prepare( $query, $post_id );
+		$term_tax_ids 	= $wpdb->get_results( $query );
+
+		if ( empty( $term_tax_ids ) ) {
+			return array();
+		}
+
+		// Now we know the term IDs, so let's find out what they are
+		$usable_term_ids = array();
+		foreach ( $term_tax_ids as $id => $term_obj ) {
+			$usable_term_ids[] = $term_obj->term_taxonomy_id;
+		}
+
+		$num_terms = count( $usable_term_ids );
+		// prepare the right amount of placeholders
+		$placeholders = array_fill( 0, $num_terms, '%d' );
+
+		// glue together all the placeholders...
+		// $format = '%d, %d, %d, %d, %d, [...]'
+		$format = implode( ', ', $placeholders );
+
+		$query			= "SELECT * FROM {$blog_prefix}terms WHERE term_id IN ({$format})";
+		$query			= $wpdb->prepare( $query, $usable_term_ids );
+		$terms 			= $wpdb->get_results( $query );
+
+		return $terms;
+
+	}/* get_program_objectives_for_post_of_site() */
+
+
+	/**
+	 * During the onboarding process we ask what program the new site is for. Programs
+	 * are a CPT on the network's main site. We grab a list of 'em
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param (bool) $usable Whether this should be a simple post_id=>post_title array
+	 * @return (array) Array of Objects of programs available on the network
+	 */
+
+	public static function get_programs_for_current_site( $usable = false ) {
+
+		// First we need to get the main site for the network of the current blog
+		$main_site_id 	= \UBC\Press\Utils::get_id_for_networks_main_site_of_blog_id( get_current_blog_id() );
+
+		// Now fetch a list of programs from the main site
+		global $wpdb;
+
+		$blog_prefix	= $wpdb->get_blog_prefix( $main_site_id );
+
+		$query			= "SELECT * FROM {$blog_prefix}posts WHERE post_type = %s AND post_status = %s";
+		$query 			= $wpdb->prepare( $query, 'program', 'publish' );
+		$programs 		= $wpdb->get_results( $query );
+
+		if ( empty( $programs ) ) {
+			return array();
+		}
+
+		if ( false === $usable ) {
+			return $programs;
+		}
+
+		$usable_programs = array();
+
+		foreach ( $programs as $id => $program_object ) {
+			$usable_programs[ $program_object->ID ] = $program_object->post_title;
+		}
+
+		return $usable_programs;
+
+	}/* get_programs_for_current_site() */
+
+
+	/**
+	 * Get the ID of the main site of the network in which the passed
+	 * $blog_id resides
+	 *
+	 * Usage: \UBC\Press\Utils::get_id_for_networks_main_site_of_blog_id( get_current_blog_id() );
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param (int) $blog_id - The ID of the blog for which we are looking for the network's main site ID
+	 * @return (int|false) The ID of the network's main site. False on failure.
+	 */
+
+	public static function get_id_for_networks_main_site_of_blog_id( $blog_id = false ) {
+
+		// get_main_site_for_network() from WPMN is required
+		if ( ! function_exists( 'get_main_site_for_network' ) ) {
+			return false;
+		}
+
+		// If $blog_id isn't passed, default to the current blog ID
+		if ( false === $blog_id ) {
+			$blog_id = get_current_blog_id();
+		}
+
+		// Sanitize
+		$blog_id = (int) $blog_id;
+
+		// If sanitization fails, you're naughty, bail
+		if ( empty( $blog_id ) ) {
+			return false;
+		}
+
+		// Check object cache
+		if ( ! $main_site_id = wp_cache_get( 'network:' . $blog_id . ':main_site_for_blog', 'site-options' ) ) {
+
+			global $wpdb;
+
+			// Fetch the site_id (network ID) for the passed blog_id
+			$query = "SELECT site_id FROM $wpdb->blogs WHERE blog_id = %d LIMIT 1";
+			$query = $wpdb->prepare( $query, $blog_id );
+			$network_id = $wpdb->get_var( $query );
+
+			// Yeah, well, this should never happen, but JIC
+			if ( empty( $network_id ) ) {
+				return false;
+			}
+
+			// Now use WPMS's get_main_site_for_network() to get the main site for this network
+			$main_site_id = get_main_site_for_network( $network_id );
+
+			// Prime the cache
+			wp_cache_add( 'network:' . $blog_id . ':main_site_for_blog', $main_site_id, 'site-options' );
+
+		}
+
+		// 🚢
+		return (int) $main_site_id;
+
+	}/* get_id_for_networks_main_site_of_blog_id() */
+
 }/* Utils */
