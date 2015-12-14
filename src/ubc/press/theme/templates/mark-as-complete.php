e04c081be416b767cc990884615d04dc03cc9776
Richard Tape <richard@iamfriendly.com>
2015-12-14 15:55:46 -0800

    ADDED a wrapper around the mark as complete area for a component, allows us to style this much more easily

--- src/ubc/press/theme/templates/mark-as-complete.php
+++ src/ubc/press/theme/templates/mark-as-complete.php
@@ -19,11 +19,13 @@ $dashicon 				= ( ! $when_completed_text ) ? 'dashicons dashicons-yes onhover' :
 $url 					= \UBC\Press\Ajax\Utils::get_ubc_press_ajax_action_url( 'mark_as_complete', true, $nonce, array( 'post_id' => $data['post_id'], 'post_type' => $data['post_type'] ) );
 ?>
 
-<a role="button" aria-label="<?php echo esc_html( $button_text ); ?>" href="<?php echo esc_url( $url ); ?>" data-nonce="<?php echo esc_html( $nonce ); ?>" data-post_id="<?php echo absint( $data['post_id'] ); ?>" class="small button radius mark-as-complete <?php echo esc_html( $button_class ); ?>">
-	<?php echo esc_html( $button_text ); ?>
-	<span class="<?php echo esc_html( $dashicon ); ?>"></span>
-</a>
+<div class="mark-as-complete-wrapper">
+	<a role="button" aria-label="<?php echo esc_html( $button_text ); ?>" href="<?php echo esc_url( $url ); ?>" data-nonce="<?php echo esc_html( $nonce ); ?>" data-post_id="<?php echo absint( $data['post_id'] ); ?>" class="small button radius mark-as-complete <?php echo esc_html( $button_class ); ?>">
+		<?php echo esc_html( $button_text ); ?>
+		<span class="<?php echo esc_html( $dashicon ); ?>"></span>
+	</a>
 
-<?php if ( $when_completed_text ) : ?>
-<span class="when_completed"><?php echo esc_html( sprintf( _x( 'Completed %s ago', '%s = human-readable time difference', \UBC\Press::get_text_domain() ), $when_completed_text ) ); ?></span>
-<?php endif; ?>
+	<?php if ( $when_completed_text ) : ?>
+	<span class="when_completed"><?php echo esc_html( sprintf( _x( 'Completed %s ago', '%s = human-readable time difference', \UBC\Press::get_text_domain() ), $when_completed_text ) ); ?></span>
+	<?php endif; ?>
+</div>
