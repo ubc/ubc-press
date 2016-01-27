ae6f66125f55c77b65c955c27db7a857f0450d1f
Richard Tape <richard@iamfriendly.com>
2016-01-27 13:44:18 -0800

    ADDED a submission_type taxonomy to submissions

--- src/ubc/press/cts/setup.php
+++ src/ubc/press/cts/setup.php
@@ -19,6 +19,9 @@ class Setup {
 		// Handout types
 		add_action( 'ubc_press_after_create_cpt', array( $this, 'ubc_press_after_create_cpt__add_handout_type' ), 10, 4 );
 
+		// Submission categories
+		add_action( 'ubc_press_after_create_cpt', array( $this, 'ubc_press_after_create_cpt__add_submission_category' ), 10, 4 );
+
 	}/* init() */
 
 
@@ -44,5 +47,30 @@ class Setup {
 		$cpt_object->register_taxonomy( 'handout_type', array( 'hierarchical' => false ) );
 
 	}/* ubc_press_after_create_cpt__add_handout_type() */
+
+
+	/**
+	 * Add submission categories which will allow us to group submission types
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param (array) $label_args - Label arguments for this post type
+	 * @param (array) $wp_args - WordPress args for this post type
+	 * @param (string) $icon - The dashicon icon string
+	 * @param (object) $cpt_object - The created whole CPT Object
+	 * @return null
+	 */
+
+	public function ubc_press_after_create_cpt__add_submission_category( $label_args, $wp_args, $icon, $cpt_object ) {
+
+		if ( 'submission' !== $cpt_object->post_type_name ) {
+			return;
+		}
+
+		// Add the Handout Type taxonomy
+		$cpt_object->register_taxonomy( 'submission_type', array( 'hierarchical' => false ) );
+
+	}/* ubc_press_after_create_cpt__add_submission_category() */
+
 
 }/* class Setup */
