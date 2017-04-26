f9c7f4d9f498980ec4eba289e01b5f9045588924
Richard Tape <richard@iamfriendly.com>
2017-04-26 16:02:43 -0700

    FIXED submission author being shown properly

--- src/UBC/Press/WPDashboard/Setup.php
+++ src/UBC/Press/WPDashboard/Setup.php
@@ -1291,7 +1291,7 @@ class Setup extends \UBC\Press\ActionsBeforeAndAfter {
 			$url 	= admin_url( "admin.php?page=gf_entries&view=entry&lid=$entry_id&id=$form_id" );
 			$graded	= get_post_meta( $submission_post_id, 'submission_grade', true );
 			$post 	= get_post( $submission_post_id );
-			$author_name = get_the_author_meta( $post->post_author );
+			$author_name = get_the_author_meta( 'display_name', $post->post_author );
 			$data['submissions'][] = array( 'title' => $title, 'url' => $url, 'graded' => $graded, 'author' => $author_name );
 		}
 
