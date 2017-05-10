cd37673851b27c90edcde45830b51fbf41a141ee
David Brabbins <david.brabbins@outlook.com>
2017-05-10 13:22:50 -0700

    MINOR html and svg changes

--- src/UBC/Press/StudentDashboard/templates/me-notes-single.php
+++ src/UBC/Press/StudentDashboard/templates/me-notes-single.php
@@ -8,9 +8,10 @@
 
 	<div id="note-toggle" class="callout">
 
-		<p class="note-meta">Filed under "<a href="<?php echo esc_url( get_permalink( $data['post_id'] ) ); ?>#section3"><?php echo esc_html( get_the_title( $data['post_id'] ) ); ?></a>".<br>
-		<small>Last updated: <?php echo esc_html( date( 'l M j, Y', $data['note_data']['when'] ) ); ?></small></p>
-
+		<h4 class="note-meta"><small>Filed under</small> "<a href="<?php echo esc_url( get_permalink( $data['post_id'] ) ); ?>#section3"><?php echo esc_html( get_the_title( $data['post_id'] ) ); ?></a>".</h4>
+		<p>
+			<small>Last updated: <?php echo esc_html( date( 'l M j, Y', $data['note_data']['when'] ) ); ?></small></p>
+		</p>
 		<button data-toggle="note-<?php echo esc_attr( $data['post_id'] ); ?>" class="button small note-button secondary">Read note</button> <a class="button small" href="<?php echo esc_url( get_permalink( $data['post_id'] ) ); ?>#section3" target="_blank">Go to note ></a>
 		<div id="note-<?php echo esc_attr( $data['post_id'] ); ?>" class="note" data-toggler=".expanded">
 			<?php echo wp_kses_post( $data['note_data']['content'] ); ?>
